ad21e4e7aed1b90709e213fee7895c7e10510f76
TishaWolfert wolvinl@yahoo.com <wolvinl@yahoo.com>
2023-05-13 20:48:36 +0200

    add pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  def home
+  end
+
   def about
+  end
+
+  def contact
+    @members = ["bibi", "angel", "coco", "russie", "grayson"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,17 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # GET localhost:3000/about
+  # add a about pages:
+  root to: "pages#home"
+  # verb "/path", to: "controllername#action"
+
+  # GET localhost:3000/about
+  # add a about pages:
   get "/about", to: "pages#about"
+  # verb "/path", to: "controllername#action"
+
+  # GET localhost:3000/about
+  # add a contact page:
+  get "/contact", to: "pages#contact"
   # verb "/path", to: "controllername#action"
 end
