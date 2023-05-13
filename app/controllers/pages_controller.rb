6c528a51ce19c5f04667913cb5bcfcf5e0334ee8
TishaWolfert wolvinl@yahoo.com <wolvinl@yahoo.com>
2023-05-13 21:08:07 +0200

    params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,13 @@ class PagesController < ApplicationController
 
   def contact
     @members = ["bibi", "angel", "coco", "russie", "grayson"]
+
+    search = params[:member] # what they typed in the form
+
+    if search
+      @members = @members.select do |member|
+        member.start_with? search.downcase
+      end
+    end
   end
 end
